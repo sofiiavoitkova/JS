1064ca1c4d1fb92545dbade72c12d3e1a4d3b9d5
Sofiia <sofiiavoitkova@gmail.com>
2024-04-05 16:25:37 +0200

    doc: add Arrow-functions

---
+++ Arrow-functions/main.js
@@ -0,0 +1,21 @@
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+
+//   ask(
+//     "Вы согласны?",
+//     function() { alert("Вы согласились."); },
+//     function() { alert("Вы отменили выполнение."); }
+//   );
+
+const ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Вы согласны?",
+    () => alert("Вы согласились."),
+    () => alert("Вы отменили выполнение.")
+);
