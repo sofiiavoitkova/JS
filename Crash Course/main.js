0ff153d38eb2711811291ddaaad067358f04186b
Sofiia <sofiiavoitkova@gmail.com>
2024-04-12 23:12:35 +0200

    doc: add 'Crash Course'

---
+++ Crash Course/main.js
@@ -0,0 +1,9 @@
+// alert("Hello World");
+// console.log("Hello World");
+// console.error("This is an warning");
+// console.warn('This is a warning');
+
+const s = "Hello World!";
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.split(""));
