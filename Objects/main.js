ae12cf2370cefe26828c13a8d3b8fa3894cc3818
Sofiia <sofiiavoitkova@gmail.com>
2024-04-15 23:47:33 +0200

    feat: add Task 5

--- Objects/main.js
+++ Objects/main.js
@@ -36,3 +36,21 @@ for (let key in salaries) {
 }
 
 console.log(sum);
+
+//Task5
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+function multiplyNumeric(obj) {
+  for (let i in obj) {
+    if (typeof obj[i] === "number") {
+      obj[i] *= 2;
+    }
+  }
+}
+
+multiplyNumeric(menu);
+console.log(menu);
