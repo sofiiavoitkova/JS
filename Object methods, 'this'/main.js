de6fc90bebada152b167eff289f885e4540a0431
Sofiia <sofiiavoitkova@gmail.com>
2024-04-17 22:46:12 +0200

    feat: add Tasks 2-3

---
+++ Object methods, 'this'/main.js
@@ -0,0 +1,38 @@
+//Task2
+let calculator = {
+  read() {
+    this.a = +prompt("a?", 0);
+    this.b = +prompt("b?", 0);
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+
+//Task3
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    alert(this.step);
+    return this;
+  },
+};
+
+ladder.up().up().down().showStep().down().showStep();
