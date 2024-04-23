706c29e16c3e701d233b196dce67603ec7246fca
Sofiia <sofiiavoitkova@gmail.com>
2024-04-23 23:37:35 +0200

    feat: add Task 7

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -97,3 +97,14 @@ let sorted = copySorted(arr);
 
 alert(sorted);
 alert(arr);
+
+//Task7
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [vasya, petya, masha];
+
+let names = users.map((user) => user.name);
+
+console.log(names); // Вася, Петя, Маша
