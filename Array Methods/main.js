6fdb932bb8375ee40a7d51cc2c8aeea0c1f2cc48
Sofiia <sofiiavoitkova@gmail.com>
2024-04-23 23:41:04 +0200

    feat: add Task 8

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -108,3 +108,26 @@ let users = [vasya, petya, masha];
 let names = users.map((user) => user.name);
 
 console.log(names); // Вася, Петя, Маша
+
+//Task8
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [vasya, petya, masha];
+
+let usersMapped = users.map((user) => ({
+  id: user.id,
+  fullName: `${user.name} ${user.surname}`,
+}));
+
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+
+console.log(usersMapped[0].id); // 1
+console.log(usersMapped[0].fullName); // Вася Пупкин
