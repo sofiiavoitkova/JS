36a1b731b01fc814e64aeec4678a4584252f0abd
Sofiia <sofiiavoitkova@gmail.com>
2024-04-23 23:34:23 +0200

    feat: add Task 5

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -82,3 +82,18 @@ let arr = [5, 2, 1, -10, 8];
 arr.sort((a, b) => b - a);
 
 alert(arr);
+
+//Task5
+function copySorted(arr) {
+  let sortedCopy = arr.slice();
+
+  sortedCopy.sort();
+
+  return sortedCopy;
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+
+alert(sorted);
+alert(arr);
