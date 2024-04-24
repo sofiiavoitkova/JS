f4754684001543d9c9b28e20609a14cb6086cbe4
Sofiia <sofiiavoitkova@gmail.com>
2024-04-24 23:07:48 +0200

    feat: add Task 9

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -131,3 +131,21 @@ usersMapped = [
 
 console.log(usersMapped[0].id); // 1
 console.log(usersMapped[0].fullName); // Вася Пупкин
+
+//Task9
+function sortByAge(arr) {
+  arr.sort((a, b) => a.age - b.age);
+}
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [vasya, petya, masha];
+
+sortByAge(arr);
+
+// теперь: [vasya, masha, petya]
+alert(arr[0].name); // Вася
+alert(arr[1].name); // Маша
+alert(arr[2].name); // Петя
