b6a686b6e4f979bf5f6f8db304aaa9728f85b8f2
Sofiia <sofiiavoitkova@gmail.com>
2024-04-24 23:45:01 +0200

    feat: add Task 13

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -249,3 +249,20 @@ let strings = [
 ];
 
 console.log(unique(strings));
+
+//Task13
+function groupById(arr) {
+  return arr.reduce(function (result, user) {
+    result[user.id] = user;
+    return result;
+  }, {});
+}
+
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+let usersById = groupById(users);
+console.log(usersById);
