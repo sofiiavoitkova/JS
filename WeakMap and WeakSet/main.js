a6a8b5722931fa254f8a3c8d4b27e25f9ca1db6e
Sofiia <sofiiavoitkova@gmail.com>
2024-04-25 23:47:31 +0200

    feat: add Task 2

---
+++ WeakMap and WeakSet/main.js
@@ -0,0 +1,10 @@
+//Task2
+let messages = [
+  { text: "Hello", from: "John" },
+  { text: "How goes?", from: "John" },
+  { text: "See you soon", from: "Alice" },
+];
+
+let readMap = new WeakMap();
+
+readMap.set(messages[0], new Date(2017, 1, 1));
