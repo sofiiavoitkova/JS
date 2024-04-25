2b801bee7e869971ef3a5bd651cf124ade85d7f1
Sofiia <sofiiavoitkova@gmail.com>
2024-04-25 23:26:23 +0200

    feat: add Task 2

--- Map and Set/main.js
+++ Map and Set/main.js
@@ -18,3 +18,35 @@ let values = [
 ];
 
 alert(unique(values));
+
+//Task2
+//the 1st way
+function aclean(arr) {
+  let map = new Map();
+
+  for (let word of arr) {
+    let sorted = arr[i].toLowerCase().split("").sort().join("");
+  }
+
+  return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert(aclean(arr));
+
+//the 2nd way
+function aclean(arr) {
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let sorted = arr[i].toLowerCase().split("").sort().join("");
+    obj[sorted] = arr[i];
+  }
+
+  return Object.values(obj);
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert(aclean(arr));
