fbbd1442d6a7fada14eac571f890a505311d7398
Sofiia <sofiiavoitkova@gmail.com>
2024-04-27 00:41:21 +0200

    feat: add Task 4

--- Date and time/main.js
+++ Date and time/main.js
@@ -2,7 +2,7 @@
 let d = new Date(2012, 1, 20, 3, 12);
 console.log(d);
 
-//Task2
+//Task3
 function getLocalDay(date) {
   let day = date.getDay();
 
@@ -15,3 +15,16 @@ function getLocalDay(date) {
 
 let date = new Date(2012, 0, 3);
 alert(getLocalDay(date));
+
+//Task4
+function getDateAgo(date, days) {
+  let dateCopy = new Date(date);
+  dateCopy.setDate(date.getDate() - days);
+  return dateCopy.getDate();
+}
+
+let date = new Date(2015, 0, 2);
+
+console.log(getDateAgo(date, 1)); // 1
+console.log(getDateAgo(date, 2)); // 31
+console.log(getDateAgo(date, 365)); // 2
