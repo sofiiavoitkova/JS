c64eee03b2e4f7960e6b86fcce99edf9231f6d1c
Sofiia <sofiiavoitkova@gmail.com>
2024-04-27 02:13:22 +0200

    feat: add another solution for Task 8

--- Date and time/main.js
+++ Date and time/main.js
@@ -79,6 +79,7 @@ function getSecondsToTomorrow() {
 }
 
 //Task8
+//the 1st way
 function formatDate(date) {
   let diff = new Date() - date;
 
@@ -102,7 +103,36 @@ function formatDate(date) {
   }
 }
 
-alert(formatDate(new Date(new Date() - 1)));
-alert(formatDate(new Date(new Date() - 30 * 1000)));
-alert(formatDate(new Date(new Date() - 5 * 60 * 1000)));
-alert(formatDate(new Date(new Date() - 86400 * 1000)));
+console.log(formatDate(new Date(new Date() - 1)));
+console.log(formatDate(new Date(new Date() - 30 * 1000)));
+console.log(formatDate(new Date(new Date() - 5 * 60 * 1000)));
+console.log(formatDate(new Date(new Date() - 86400 * 1000)));
+
+//the 2nd way
+function formatDate(date) {
+  let dayOfMonth = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let diffMs = new Date() - date;
+  let diffSec = Math.round(diffMs / 1000);
+  let diffMin = diffSec / 60;
+  let diffHour = diffMin / 60;
+
+  year = year.toString().slice(-2);
+  month = month < 10 ? "0" + month : month;
+  dayOfMonth = dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth;
+  hour = hour < 10 ? "0" + hour : hour;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  if (diffSec < 1) {
+    return "right now";
+  } else if (diffMin < 1) {
+    return `${diffSec} sec. ago`;
+  } else if (diffHour < 1) {
+    return `${diffMin} min. ago`;
+  } else {
+    return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`;
+  }
+}
