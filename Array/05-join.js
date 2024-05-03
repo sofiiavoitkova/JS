fef0d38829fad35801193439937f024dacecb2a5
Sofiia <sofiiavoitkova@gmail.com>
2024-05-03 12:32:08 +0200

    doc: add 05-join.js

---
+++ Array/05-join.js
@@ -0,0 +1,16 @@
+/**
+ * Task description: Write a simple JavaScript program to join all elements of the following array into a string.
+ * Sample array : myColor = ["Red", "Green", "White", "Black"];
+ * Expected Result: "Red,Green,White,Black"
+                    "Red,Green,White,Black"
+                    "Red+Green+White+Black"
+ */
+let myColor = ["Red", "Green", "White", "Black"];
+
+let arr1 = myColor.join(",");
+
+let arr2 = myColor.join("+");
+
+console.log(arr1);
+console.log(arr1);
+console.log(arr2);
