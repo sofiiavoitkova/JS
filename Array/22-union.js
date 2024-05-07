add588c581b6e6f5d677f76d30fe44f5f131df6f
Sofiia <sofiiavoitkova@gmail.com>
2024-05-08 00:40:14 +0200

    doc: add 22-union.js

---
+++ Array/22-union.js
@@ -0,0 +1,16 @@
+/**
+ * Task description: Write a JavaScript program to compute the union of two arrays
+ * Expected Result: [1, 2, 3, 10, 100]
+ * @param {Array} arr - The input array
+ * @returns {Array} - An array containing the duplicate values
+ */
+
+function union(array1, array2) {
+  let union = array1.concat(array2);
+
+  union.sort((a, b) => a - b);
+
+  return Array.from(new Set(union));
+}
+
+console.log(union([1, 2, 3], [100, 2, 1, 10]));
