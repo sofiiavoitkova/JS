eff49d5c4328822be22de27d6ab95ac462c51734
Sofiia <sofiiavoitkova@gmail.com>
2024-05-09 23:44:18 +0200

    doc: add 31-remove.js

---
+++ Array/31-remove.js
@@ -0,0 +1,17 @@
+/**
+ * Task description: Write a JavaScript function to remove a specific element from an array
+ * Expected Result:  [2, 9, 6]
+ * @param {Array} arr - The input array
+ * @param {number} n - The element to remove
+ * @returns {Array} - An array without a specific element
+ */
+function remove_array_element(arr, n) {
+  let arr1 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== n) {
+      arr1.push(arr[i]);
+    }
+  }
+  return arr1;
+}
+console.log(remove_array_element([2, 5, 9, 6], 5));
