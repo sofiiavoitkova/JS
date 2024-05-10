48f3db4b40a154380d9127f5a688068e9d0532ed
Sofiia <sofiiavoitkova@gmail.com>
2024-05-10 23:30:56 +0200

    feat: add another solution

--- Array/32-find.js
+++ Array/32-find.js
@@ -1,10 +1,11 @@
 /**
  * Task description: Write a JavaScript function to find an array containing a specific element.
- * Expected Result:  [True]
+ * Expected Result: [True]
  * @param {Array} arr - The input array
  * @param {number} n - The element to search for
  * @returns {boolean} - True if the element is found, otherwise false
  */
+//the 1st way
 function contains(arr, n) {
   let arr1 = [];
 
@@ -20,3 +21,8 @@ function contains(arr, n) {
 arr = [2, 5, 9, 6];
 
 console.log(contains(arr, 5));
+
+//the 2nd way
+function contains(arr, n) {
+  return arr.includes(n);
+}
