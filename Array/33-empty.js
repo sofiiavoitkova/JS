7ae40ad824c37d02f41dcd5f406c632adbbe735a
Sofiia <sofiiavoitkova@gmail.com>
2024-05-11 00:02:00 +0200

    feat: remove tags

--- Array/33-empty.js
+++ Array/33-empty.js
@@ -1,9 +1,6 @@
 /**
  * Task description: Write a JavaScript script to empty an array while keeping the original.
  * Expected Result: empty array
- * @param {Array} arr - The input array
- * @param {number} n - The element to search for
- * @returns {boolean} - True if the element is found, otherwise false
  */
 let arr = [2, 5, 9, 6];
 
