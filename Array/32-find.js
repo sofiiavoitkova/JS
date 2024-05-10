cbd1ef616e6f95f1416424b567be29a75f51baa0
Sofiia <sofiiavoitkova@gmail.com>
2024-05-10 23:29:14 +0200

    doc: add 32-find.js

---
+++ Array/32-find.js
@@ -0,0 +1,22 @@
+/**
+ * Task description: Write a JavaScript function to find an array containing a specific element.
+ * Expected Result:  [True]
+ * @param {Array} arr - The input array
+ * @param {number} n - The element to search for
+ * @returns {boolean} - True if the element is found, otherwise false
+ */
+function contains(arr, n) {
+  let arr1 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === n) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+arr = [2, 5, 9, 6];
+
+console.log(contains(arr, 5));
