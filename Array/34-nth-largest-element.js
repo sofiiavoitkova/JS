03647b914a475824c82705bd2e8d3b5268a6bb4b
Sofiia <sofiiavoitkova@gmail.com>
2024-05-11 22:50:17 +0200

    doc: add 34-nth-largest-element.js

---
+++ Array/34-nth-largest-element.js
@@ -0,0 +1,14 @@
+/**
+ * Task description: Write a JavaScript function to get the nth largest element from an unsorted array.
+ * Expected Result: 89
+ * @param {Array} arr - The unsorted array
+ * @param {number} n - The position of the nth largest element
+ * @returns {number} - The nth largest element
+ */
+function nthLargest(arr, n) {
+  arr.sort((a, b) => b - a);
+
+  return arr[n - 1];
+}
+
+console.log(nthLargest([43, 56, 23, 89, 88, 90, 99, 652], 4));
