b9b50849fce2409389b74a22bc9b8e250dfcf608
Sofiia <sofiiavoitkova@gmail.com>
2024-05-12 03:28:04 +0200

    doc: add 41-range-between.js

---
+++ Array/41-range-between.js
@@ -0,0 +1,20 @@
+/**
+ * Task description: Write a JavaScript function to generate an array between two integers of 1 step length.
+ * Expected results:[4, 5, 6, 7]
+ *                  [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+ * @param {number} s - The starting integer
+ * @param {number} l - The ending integer
+ * @returns {Array} - The array containing numbers between two integers
+ */
+function rangeBetwee(s, l) {
+  let result = [];
+
+  for (let i = s; i <= l; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+console.log(rangeBetwee(4, 7));
+console.log(rangeBetwee(-4, 7));
