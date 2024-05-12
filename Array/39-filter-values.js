88905038f280dd3e6d40cf15bcc9feffa659fba9
Sofiia <sofiiavoitkova@gmail.com>
2024-05-12 02:53:33 +0200

    doc: add 39-filter-values.js

---
+++ Array/39-filter-values.js
@@ -0,0 +1,14 @@
+/**
+ * Task description: Write a JavaScript function to move an array element from one position to another.
+ * Expected results: [58, "abcd", true]
+
+ * @param {Array} arr - The input array
+ * @param {number} from - The index of the element to move
+ * @param {number} to - The position to move the element to
+ * @returns {Array} - The array with the element moved
+ */
+function filter_array_values(arr) {
+  return arr.filter((val) => val);
+}
+
+console.log(filter_array_values([58, "", "abcd", true, null, false, 0]));
