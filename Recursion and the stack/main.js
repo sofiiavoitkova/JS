5ba2254f5ebf942ebb1f75dd5d2963f14ccf84be
Sofiia <sofiiavoitkova@gmail.com>
2024-05-14 23:02:48 +0200

    feat: add another solution

--- Recursion and the stack/main.js
+++ Recursion and the stack/main.js
@@ -97,6 +97,7 @@ alert(fib(77));
  * @param {Object} list - The linked list to print
  */
 
+//The 1st solution
 let list = {
   value: 1,
   next: {
@@ -117,6 +118,31 @@ function printList(list) {
   while (tmp) {
     console.log(tmp.value);
     tmp = tmp.next;
+  }
+}
+
+printList(list);
+
+//The 2nd solution
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+function printList(list) {
+  console.log(list.value);
+
+  if (list.next) {
+    printList(list.next);
   }
 }
 
