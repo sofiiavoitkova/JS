39613a1025a59ca2f202eaaf33b0fbfde4e9db03
Sofiia <sofiiavoitkova@gmail.com>
2024-05-21 22:20:00 +0200

    refactor: rename function

--- Array/03-get-element.js
+++ Array/03-get-element.js
@@ -1,16 +1,16 @@
 /**
- * Task description: Write a JavaScript function to get the first element of an array. 
+ * Task description: Write a JavaScript function to get the first element of an array.
  * Passing the parameter 'n' will return the first 'n' elements of the array.
  * Expected Result: 7
-                    []
-                    [7, 9, 0]
-                    [7, 9, 0, -2]
-                    []
+ *                  []
+ *                  [7, 9, 0]
+ *                  [7, 9, 0, -2]
+ *                  []
  * @param {Array} arr - The input array
  * @param {number} n - The number of elements to return
  * @returns {Array} The first 'n' elements of the array
  */
-function first(arr, n) {
+function getFirstElement(arr, n) {
   if (n < 0) {
     return [];
   }
@@ -22,8 +22,8 @@ function first(arr, n) {
   return arr.slice(0, n);
 }
 
-console.log(first([7, 9, 0, -2]));
-console.log(first([], 3));
-console.log(first([7, 9, 0, -2], 3));
-console.log(first([7, 9, 0, -2], 6));
-console.log(first([7, 9, 0, -2], -3));
+console.log(getFirstElement([7, 9, 0, -2]));
+console.log(getFirstElement([], 3));
+console.log(getFirstElement([7, 9, 0, -2], 3));
+console.log(getFirstElement([7, 9, 0, -2], 6));
+console.log(getFirstElement([7, 9, 0, -2], -3));
