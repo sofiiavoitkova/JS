a9cc0ac2ade0638b97c51eaed9108e0633545a89
Sofiia <sofiiavoitkova@gmail.com>
2024-06-01 22:57:12 +0200

    doc: add Task 3

---
+++ Decorators and forwarding, call, apply/Task3.js
@@ -0,0 +1,31 @@
+/**
+ * Task description: The result of debounce(f, ms) decorator is a wrapper that suspends calls to f until
+ * there’s ms milliseconds of inactivity (no calls, “cooldown period”), then invokes f once with the latest arguments.
+ * @param {Function} func - The function to debounce
+ * @param {number} wait - The number of milliseconds to delay
+ * @returns {Function} - A new debounced function
+ */
+
+function debounce(func, wait) {
+  let timeout;
+
+  return function (...args) {
+    const context = this;
+
+    const later = () => {
+      timeout = null;
+      func.apply(context, args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
+const debounced = debounce((message) => {
+  console.log(message);
+}, 1000);
+
+debounced("a");
+setTimeout(() => debounced("b"), 200);
+setTimeout(() => debounced("c"), 500);
