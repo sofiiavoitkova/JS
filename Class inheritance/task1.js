745ffe680967570835b3513adbd9f79e9c48d629
Sofiia <sofiiavoitkova@gmail.com>
2024-06-23 23:32:32 +0200

    doc: add Task 1

---
+++ Class inheritance/task1.js
@@ -0,0 +1,16 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit");
+
+console.log(rabbit.name);
