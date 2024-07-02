8334162a857956a7e04dcf072b792c2386d238dd
Sofiia <sofiiavoitkova@gmail.com>
2024-07-02 08:25:05 +0200

    doc: add Task 3

---
+++ Async, await/task3.js
@@ -0,0 +1,16 @@
+async function wait() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return 10;
+}
+
+function f() {
+  wait()
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+f();
